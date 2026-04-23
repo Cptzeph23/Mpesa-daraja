67f7513f74c92ee59310716451ed90b3dfbf698a
Cptzeph <asekazephaniah@gmail.com>
2026-04-23 16:34:18 +0300

    urls

--- RootDir/mpesa/urls.py
+++ RootDir/mpesa/urls.py
@@ -6,4 +6,7 @@ app_name = "mpesa"
 urlpatterns = [
     path("stk-push/", STKPushView.as_view(), name="stk-push"),
     path("callback/", MpesaCallbackView.as_view(), name="callback"),
+    path("", payment_page, name="payment-page"),
+    path("stk-push/", STKPushView.as_view(), name="stk-push"),
+    
 ]
